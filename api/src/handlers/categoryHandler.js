b3817af850f9c0c5a189d6f452d96d8d21aa08ab
olivio-git <subelzaolivitocabezas@gmail.com>
2023-03-14 13:42:45 -0400

    update routes y handler

---
+++ api/src/handlers/categoryHandler.js
@@ -0,0 +1,48 @@
+const CategoryService = require('./../services/categoryService');
+const service = new CategoryService();
+
+class CategoryHandler {
+    constructor() {}
+    async getAllCategories (req,res,next){
+        try {
+            const categorys = await service.getAllCategories();
+            res.json(categorys);
+        } catch (error) {
+            next(error.message)
+        }
+    }
+    async createCategory (req,res,next){
+        try {
+            const categorys = await service.createCategory(req.body);
+            res.json(categorys);
+        } catch (error) {
+            next(error.message)
+        }
+    }
+    async getCategoryById (req,res,next){
+        try {
+            const categorys = await service.getCategoryById(req.params.id);
+            res.json(categorys);
+        } catch (error) {
+            next(error.message)
+        }
+    }
+    async deleteCategory (req,res,next){
+        try {
+            const categorys = await service.getCategoryById(req.params.id);
+            res.json(categorys);
+        } catch (error) {
+            next(error.message)
+        }
+    }
+    async reactivateCategory (req,res,next){
+        try {
+            const categorys = await service.getCategoryById(req.params.id);
+            res.json(categorys);
+        } catch (error) {
+            next(error.message)
+        }
+    }
+}
+
+module.exports = CategoryHandler
