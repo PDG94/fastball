fb0c0e5dd7971e53300dabb355a2fd95c2ff7ec0
olivio-git <subelzaolivitocabezas@gmail.com>
2023-03-14 02:35:32 -0400

    categoryRoute done, categoryService done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const userRouter = require('./../routes/userRoute')
+const categoryRouter = require('./../routes/categoryRoute')
+
 
 function routerApi(app){
     const router = express.Router();
     app.use('/api/', router);
     router.use('/users', userRouter);
+    router.use('/category', categoryRouter);
 };
 
 module.exports = routerApi;
