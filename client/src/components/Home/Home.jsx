1f86247c08acdf5a4ac5d215950d0981ec22467c
olivio-git <subelzaolivitocabezas@gmail.com>
2023-03-15 16:02:51 -0400

    cración de rutas en app

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
+import CategoryButtons from "../categoryButtons.jsx/CategoryButtons"
+import Navbar from "../Navbar/Navbar"
+
 const Home = ()=> {
     return(
         <div>
-            <h1>Estoy en Home</h1>
+            <Navbar></Navbar>
+            {/* Aqui ira carrousel */}
+            <CategoryButtons></CategoryButtons>
         </div>
     )
 }
