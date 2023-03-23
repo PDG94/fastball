42880c8fc9b1ec2d867443107c47d9236de49163
PDG94 <ranekpdg@gmail.com>
2023-03-22 22:48:24 -0300

    fase 1 user dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import UserConnection from './components/UserConnection/UserConnection'
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Detail from './components/Detail/Detail';
+import Profile from './components/UserDashboard/Profile'
 // import RegisterProduct from './components/RegisterProduct/RegisterProduct'
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -26,6 +27,8 @@ function App() {
         <Route path='/register' element={<UserConnection isLogin={false} />} />
         <Route path='/details/:id' element={<Detail />} />
         <Route path='/create' element={<ContainerRegiterPRoduct />} />
+
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </>
   );
