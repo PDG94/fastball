506ab96b55ad11372e43e89b125a851038250296
PDG94 <ranekpdg@gmail.com>
2023-03-22 23:09:40 -0300

    errorhandler temporally disabled backed

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@ const server = require("./src/app");
 const {conn} = require("./src/bd/db");
 require ('dotenv').config()
 const {PORT} = process.env
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./../api/src/middleware/errorHandler');
+// const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./../api/src/middleware/errorHandler');
 
 //Middlewares
-server.use(logErrors);
-server.use(errorHandler);
-server.use(boomErrorHandler);
-server.use(ormErrorHandler);
+// server.use(logErrors);
+// server.use(errorHandler);
+// server.use(boomErrorHandler);
+// server.use(ormErrorHandler);
 
 conn.sync({alter:true}).then(()=>{
     server.listen(PORT, () =>{
