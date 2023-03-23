b2f016a24fd9825e5bb723fd1d2e0b75b7d38249
PDG94 <ranekpdg@gmail.com>
2023-03-22 23:38:27 -0300

    fixes for new front deploy

--- client/src/components/UserConnection/Login.jsx
+++ client/src/components/UserConnection/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { Link, useNavigate } from 'react-router-dom'
 // import logGoogle from './../../images/google.svg'
