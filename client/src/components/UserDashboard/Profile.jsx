4f5dd91feb8e3bc3377a54fa49ca437842e86b54
PDG94 <ranekpdg@gmail.com>
2023-03-26 17:07:11 -0300

    functioning with a bit of styles, needs more

--- client/src/components/UserDashboard/Profile.jsx
+++ client/src/components/UserDashboard/Profile.jsx
@@ -3,80 +3,97 @@ import { useDispatch, useSelector } from "react-redux";
 // import { getServiceUser } from "../../../redux/actions/usersActions";
 
 export default function DashboardUser() {
-  const dispatch = useDispatch();
 
-  const token = localStorage.getItem("token");
+  // const dispatch = useDispatch();
+  // const token = localStorage.getItem("token");
 
-  const id = useSelector((state) => state.users.userID);
-  const { name, lastname, email, profilePic, address, contry, city } = useSelector(
+  const { _id, name, lastname, email, profilePic, address, contry, city } = useSelector(
     (state) => state.user
   );
 
   // useEffect(() => {
-  //   dispatch(getServiceUser(id, token));
+    // dispatch(getServiceUser(_id, token));
   // }, [dispatch, id, token]);
 
   return (
-    <div className="">
-      <div className="">
-        <div className="">
-          <img
-            className=""
-            src={profilePic}
-            alt=""
-          />
+    <div className='mx-auto min-height-full flex'>
+            <div className='hidden lg:block relative flex-1 h-full'>
+                <img src={'https://res.cloudinary.com/dviri5ov1/image/upload/c_fill,f_auto,h_570,q_auto,w_700/v1679507441/fastball/system/login_lcywpk.webp'} alt="loginImage" className='w-[99%] h-full' />
+            </div>
+            <div className='flex-1 flex flex-col px-4 sm:px-6 lg:flex-none mt-8'>
+                <div className='mx-auto w-full max-w-sm lg:max-w-lg lg:w-[100rem]'>
+                    <div className='flex items-center'>
+                        <div className='text-center lg:text-left'>
+                            {/* <img src={ImgLogo} alt="LogoFastBall" className='bg-blue-700 h-12 w-auto m-auto lg:m-0' />                           */}
+                        </div>
+                        
+                        <div className="">
+                          <div className="">
+                            <div className="">
+                              <img
+                                className=""
+                                src={profilePic}
+                                alt=""
+                              />
+                            </div>
+
+                            <div className="">
+                              <span className="">
+                                <b>My Info</b>
+                              </span>
+                              <br />
+
+                              <p className="">
+                                Name
+                                <br /> 
+                                <b>{name}</b>            
+                              </p>
+                              <br />
+
+                              <p className="">
+                                Surname
+                                <br />
+                                <b>{lastname}</b>
+                              </p>
+                              <br />
+
+                              <p className="">
+                                E-mail
+                                <br /> 
+                                <b>{email}</b>
+                              </p>
+                              <br />
+
+                              <p className="">
+                                Adress
+                                <br /> 
+                                <b>{address}</b>
+                              </p>
+                              <br />
+
+                              <p className="">
+                                Country
+                                <br /> 
+                                <b>{contry}</b>
+                              </p>
+                              <br />
+
+                              <p className="">
+                                City
+                                <br /> 
+                                <b>{city}</b>
+                              </p>
+                              <br />
+                              
+                            </div>      
+                          </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div className="">
-          <span className="">
-            <b>My Info</b>
-          </span>
-          <br />
 
-          <p className="">
-            Name
-            <br /> 
-            <b>{name}</b>            
-          </p>
-          <br />
-
-          <p className="">
-            Surname
-            <br />
-            <b>{lastname}</b>
-          </p>
-          <br />
-
-          <p className="">
-            E-mail
-            <br /> 
-            <b>{email}</b>
-          </p>
-          <br />
-
-          <p className="">
-            Adress
-            <br /> 
-            <b>{address}</b>
-          </p>
-          <br />
-
-          <p className="">
-            Country
-            <br /> 
-            <b>{contry}</b>
-          </p>
-          <br />
-
-          <p className="">
-            City
-            <br /> 
-            <b>{city}</b>
-          </p>
-          <br />
-          
-        </div>      
-      </div>
-    </div>
   );
 }
--- client/src/components/UserDashboard/UpdateProfile.jsx
+++ client/src/components/UserDashboard/UpdateProfile.jsx
@@ -103,94 +103,109 @@ export default function UpdateInfoUser() {
   };
 
   return (
-    <div className="">
-      <form className="" onSubmit={submitHandler}>
-        <Link to={"/profile"}>
-          <button className="">Back</button>
-        </Link>
-        <div className="">
-          <h1 className="">Update User</h1>
-
-          <div className="">
-            <span className="">Update Name</span>
-            <label className="">
-            </label>
-            <input
-              className=""
-              type="text"
-              placeholder="New Name"
-              value={form.name}
-              name="name"
-              onChange={(e) => handlerChange(e)}
-            />
-          </div>
-
-          <div className="">
-            <span className="">Update Surname</span>
-            <label className="">
-            </label>
-            <input
-              className=""
-              type="text"
-              placeholder="New Surname"
-              value={form.lastName}
-              name="lastName"
-              onChange={(e) => handlerChange(e)}
-            />
-          </div>
-
-          <div className="">
-            <span className="">Update Country</span>
-            <label className="">
-            </label>
-          </div>
-
-          <div className="">
-            <span className="">Update City</span>
-            <label className="">
-            </label>
-            <input
-              className=""
-              type="text"
-              placeholder="New City"
-              value={form.city}
-              name="city"
-              onChange={(e) => handlerChange(e)}
-            />
-          </div>         
-
-          <div className="">
-            <span className="">Update Address</span>
-            <label className="">
-            </label>
-            <input
-              className=""
-              type="text"
-              placeholder="New Address"
-              value={form.address}
-              name="address"
-              onChange={(e) => handlerChange(e)}
-            />
-          </div>   
-
-
-
-          <div className="">
-            <span className="">Update profile image</span>
-            {/* <input
-              id="file-up"
-              onChange={(e) => changing(e)}
-              type="file"
-              style={{ display: "none" }}
-            />
-            <div id="infoUp"></div> */}
-          </div>
-
-          <button type="submit" className="">
-            SUBMIT
-          </button>
-        </div>
-      </form>
+    <div className='mx-auto min-height-full flex'>
+    <div className='hidden lg:block relative flex-1 h-full'>
+        <img src={'https://res.cloudinary.com/dviri5ov1/image/upload/c_fill,f_auto,h_710,q_auto,w_700/v1679507442/fastball/system/register_gpmyxn.webp'} alt="loginImage" className='w-[99%] h-full' />
     </div>
+    <div className='flex-1 flex flex-col px-4 sm:px-6 lg:flex-none mt-8'>
+        <div className='mx-auto w-full max-w-sm lg:max-w-lg lg:w-[100rem]'>
+            <div className='flex items-center'>
+                <div className='text-center lg:text-left'>
+                    {/* <img src={ImgLogo} alt="LogoFastBall" className='bg-blue-700 h-12 w-auto m-auto lg:m-0' />                           */}
+                </div>
+
+                      <div className="">
+                        <form className="" onSubmit={submitHandler}>
+                          <Link to={"/profile"}>
+                            <button className="">Back</button>
+                          </Link>
+                          <div className="">
+                            <h1 className="">Update User</h1>
+
+                            <div className="">
+                              <span className="">Update Name</span>
+                              <label className="">
+                              </label>
+                              <input
+                                className=""
+                                type="text"
+                                placeholder="New Name"
+                                value={form.name}
+                                name="name"
+                                onChange={(e) => handlerChange(e)}
+                              />
+                            </div>
+
+                            <div className="">
+                              <span className="">Update Surname</span>
+                              <label className="">
+                              </label>
+                              <input
+                                className=""
+                                type="text"
+                                placeholder="New Surname"
+                                value={form.lastName}
+                                name="lastName"
+                                onChange={(e) => handlerChange(e)}
+                              />
+                            </div>
+
+                            <div className="">
+                              <span className="">Update Country</span>
+                              <label className="">
+                              </label>
+                            </div>
+
+                            <div className="">
+                              <span className="">Update City</span>
+                              <label className="">
+                              </label>
+                              <input
+                                className=""
+                                type="text"
+                                placeholder="New City"
+                                value={form.city}
+                                name="city"
+                                onChange={(e) => handlerChange(e)}
+                              />
+                            </div>         
+
+                            <div className="">
+                              <span className="">Update Address</span>
+                              <label className="">
+                              </label>
+                              <input
+                                className=""
+                                type="text"
+                                placeholder="New Address"
+                                value={form.address}
+                                name="address"
+                                onChange={(e) => handlerChange(e)}
+                              />
+                            </div>   
+
+
+
+                            <div className="">
+                              <span className="">Update profile image</span>
+                              {/* <input
+                                id="file-up"
+                                onChange={(e) => changing(e)}
+                                type="file"
+                                style={{ display: "none" }}
+                              />
+                              <div id="infoUp"></div> */}
+                            </div>
+
+                            <button type="submit" className="">
+                              SUBMIT
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                 </div>
+                </div>
+            </div>
+        </div>
   );
 }
