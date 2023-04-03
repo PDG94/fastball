259578504e09ba253891b6939d65ca8df03a8fa4
PDG94 <ranekpdg@gmail.com>
2023-04-02 22:44:46 -0300

    orders user fase 2

--- client/src/components/UserDashboard/OrderHistory.jsx
+++ client/src/components/UserDashboard/OrderHistory.jsx
@@ -65,8 +65,7 @@ const OrderHistory = () => {
                           {index + 1}
                         </td>
                         <td
-                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                          onClick={fecha}
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"                          
                         >
                           {createdAt}
                         </td>
