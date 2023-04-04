5487e3920678b76d088ed40f3e45c23149290c56
Jorge Kerk <jorgekerk@gmail.com>
2023-04-04 07:48:39 -0300

    info carousel

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,16 +1,31 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { ViewCarousel } from './ViewCarousel'
 import ComentaryCarousel from './ComentaryCarousel'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllUsers } from '../../reduxToolkit/actions/userActions';
+import { fetchCategory } from '../../reduxToolkit/actions/categoryAction';
 
-const formatImg = 'c_fill,h_1000,w_3000'
-const slidesCarousel = [
+export const Carousel = () => {  
+  const dispatch = useDispatch();
+  const [users, setUsers] = useState([])
+  const { allCategories } = useSelector( state => state.category )
+  const { allProducts } = useSelector( state => state.product )
+
+  useEffect(() => {
+    dispatch(getAllUsers()).then((response) => setUsers(response.payload))
+    dispatch(fetchCategory())
+
+  }, [dispatch])
+  
+  const formatImg = 'c_fill,h_1000,w_3000'
+  const slidesCarousel = [
     {   
         img: `https://res.cloudinary.com/dviri5ov1/image/upload/${formatImg}/v1679507441/fastball/system/img1_dba101.webp`, 
-        coment: <ComentaryCarousel text={'More than 100 users have been interested in this page !!!'}/>
+        coment: <ComentaryCarousel text={`${users.length>0?users.length:0} users have been interested in this page !!!`}/>
     },
     {   
         img: `https://res.cloudinary.com/dviri5ov1/image/upload/${formatImg}/v1679507442/fastball/system/img2_g0rwfp.webp`, 
-        coment: <ComentaryCarousel text={'We have 11 categories of sports products !!!'}/>
+        coment: <ComentaryCarousel text={`We have ${allCategories.length>0?allCategories.length:0} categories of sports products and ${allProducts.length>0?allProducts.length:0} products of excellent quality!!!`}/>
     },
     {   
         img: `https://res.cloudinary.com/dviri5ov1/image/upload/${formatImg}/v1679507442/fastball/system/img3_fylx1i.webp`, 
@@ -26,15 +41,14 @@ const slidesCarousel = [
     },
   ]
 
-export const Carousel = () => {
-  return    <div className='max-w-fit mx-auto px-12'>
-                <ViewCarousel>
-                    { slidesCarousel.map( (slide, ind) =>{
-                        return <Fragment key={ind}>
-                            <h1>{slide.coment}</h1>
-                            <img className="object-cover h-[80vh]" src={slide.img} alt={`img carousel ${ind}`} /> 
-                        </Fragment>
-                    })}
-                </ViewCarousel>
-            </div>
+  return  <div className='max-w-fit mx-auto px-12'>
+            <ViewCarousel>
+              { slidesCarousel.map( (slide, ind) =>{
+                return <Fragment key={ind}>
+                  <h1>{slide.coment}</h1>
+                  <img className="object-cover h-[80vh]" src={slide.img} alt={`img carousel ${ind}`} /> 
+                </Fragment>
+              })}
+            </ViewCarousel>
+          </div>
 }
