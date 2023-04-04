f1666643e3ccabe01a8083d745fcdadc6a622289
PDG94 <ranekpdg@gmail.com>
2023-04-04 11:14:30 -0300

    fix quick order history

--- client/src/components/UserDashboard/orders/OrderHistory.jsx
+++ client/src/components/UserDashboard/orders/OrderHistory.jsx
@@ -18,7 +18,13 @@ const OrderHistory = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
 
-  const filteredOrders = orders.filter((order) => order.userID1 === userID1);
+  const filteredOrders = orders.filter((order) => order.UserId === userID1);
+
+  const info = () => {
+    console.log("111",orders)
+    console.log("222",filteredOrders)
+    console.log("333", userID1)
+  };
 
   const Clickk = (id) => {
 
@@ -31,20 +37,20 @@ const OrderHistory = () => {
         <Sidebar />
         <div className="homeContainer">
           
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3" onClick={info}>
                       index
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                       Date
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                       Order Amount
                     </th>
-                    <th scope="col" class="px-6 py-3">
+                    <th scope="col" className="px-6 py-3">
                       Order ID
                     </th>
                   </tr>
